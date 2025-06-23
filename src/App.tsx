d9860a6fbc5ff6b6f40108f6207752b45c3ac843
Patrik Ivan <patrik.ivan@six-group.com>
2025-06-23 18:22:29 +0200

    Connecting to backend - 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => setUsers(res.data))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []); // Empty array to avoid infinite looping
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import userService, { User } from "../services/user-service";
+import { CanceledError } from "../services/api-client";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const { request, cancel } = userService.getAll<User>();
+    request
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
+    return () => cancel();
+  }, []); // Empty array to avoid infinite looping
+
+  return { users, error, isLoading, setUsers, setError, setLoading };
+};
+
+export default useUsers;
