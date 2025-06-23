3585a125577d60a53ea17685b6fce952dc1eb89b
Patrik Ivan <patrik.ivan@six-group.com>
2025-06-23 15:01:31 +0200

    Finishing form for the ExpenseTracker mini-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -17,6 +19,13 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,3 +1,5 @@
+import categories from "../categories";
+
 interface Props {
   onSelectCategory: (category: string) => void;
 }
@@ -9,9 +11,11 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
       onChange={(event) => onSelectCategory(event.target.value)}
     >
       <option value="">All categories</option>
-      <option value="Groceries">Groceries</option>
-      <option value="Utilities">Utilities</option>
-      <option value="Entertainment">Entertainment</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
     </select>
   );
 };
