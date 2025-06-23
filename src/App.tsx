5a4c0053352755177d57ce72052d29907d060b35
Patrik Ivan <patrik.ivan@six-group.com>
2025-06-23 16:59:37 +0200

    Connecting to backend - 8

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,25 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []); // Empty array to avoid infinite looping
 
-  return <ul>
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-  </ul>;
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
